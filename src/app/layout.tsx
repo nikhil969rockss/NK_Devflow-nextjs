e74ffd2dd3166313ff1ec6f25fb2c3514ded4a41
Nikhil Randhawa <ns9013604867@gmail.com>
2026-05-27 19:35:59 +0530

    add sign-up and  sign-in auth route structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from 'next';
 import { Inter, Space_Grotesk } from 'next/font/google';
 import './globals.css';
 import { ThemeProvider } from 'next-themes';
-import Navbar from '@/components/navigation';
 
 const inter = Inter({
   variable: '--font-inter',
@@ -32,7 +31,6 @@ export default function RootLayout({
     >
       <body className="min-h-full flex flex-col">
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-          <Navbar />
           {children}
         </ThemeProvider>
       </body>
