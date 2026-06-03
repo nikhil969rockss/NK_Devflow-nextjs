ae7056ea3d775cdf8f298ad9cee166f1eda02ce6
Nikhil Randhawa <ns9013604867@gmail.com>
2026-06-03 19:36:24 +0530

    implement the right side bar

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,5 +1,6 @@
 import LeftSideBar from '@/components/LeftSideBar';
 import Navbar from '@/components/navigation';
+import RightSideBar from '@/components/RightSideBar';
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
@@ -10,6 +11,7 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
         <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max:md:pb-14 sm:px-14">
           <div className="mx-auto w-full max-w-5xl">{children}</div>
         </section>
+        <RightSideBar />
       </div>
     </main>
   );
--- src/constants/route.ts
+++ src/constants/route.ts
@@ -2,4 +2,6 @@ export const ROUTES = {
   HOME: '/',
   SIGN_UP: '/sign-up',
   SIGN_IN: '/sign-in',
+  QUESTIONS: (id: string) => `/questions/${id}`,
+  TAGS: (_id: string) => `/tags/${_id}`,
 };
